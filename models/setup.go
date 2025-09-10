56be69d8aaa0905d8e6b2acc6dd36062e592d42e
Rifqi RIfaldi <rifqirifaldi94@gmail.com>
2025-09-10 13:31:45 +0700

    setup railway

--- models/setup.go
+++ models/setup.go
@@ -1,25 +1,49 @@
 package models
 
 import (
+	"fmt"
+	"log"
+	"net/url"
+	"os"
+
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
-// membuat variabel global untuk database
 var DB *gorm.DB
 
 func ConnectDatabase() {
-	// menghubungkan dengan database mysql
-	db, err := gorm.Open(mysql.Open("mysql://root:cfQJevUGGDUfPbvfXhImdnTvCbiwdHTd@mysql.railway.internal:3306/railway"))
+	mysqlURL := os.Getenv("mysql://root:cfQJevUGGDUfPbvfXhImdnTvCbiwdHTd@mysql.railway.internal:3306/railway")
 
-	//cek koneksi ke database
-	if err != nil {
-		panic(err)
+	var dsn string
+	if mysqlURL != "" {
+		// parse mysql://user:pass@host:port/dbname
+		parsedURL, err := url.Parse(mysqlURL)
+		if err != nil {
+			log.Fatal("Invalid MYSQL_URL:", err)
+		}
+
+		user := parsedURL.User.Username()
+		pass, _ := parsedURL.User.Password()
+		host := parsedURL.Hostname()
+		port := parsedURL.Port()
+		dbname := parsedURL.Path[1:] // buang '/' di depan
+
+		dsn = fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+			user, pass, host, port, dbname)
+	} else {
+		// fallback ke lokal
+		dsn = "root:root@tcp(localhost:8889)/rest_api_go?charset=utf8mb4&parseTime=True&loc=Local"
 	}
 
-	// migrasi tabel jika tabel di database kosong
-	db.AutoMigrate(&Book{})
-	db.AutoMigrate(&Author{})
-	db.AutoMigrate(&Member{})
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Failed to connect to database:", err)
+	}
+
+	// auto migrate
+	db.AutoMigrate(&Book{}, &Author{}, &Member{})
 	DB = db
+
+	log.Println("Database connected 🚀")
 }
