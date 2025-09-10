f7db1967b1bc18608260fbdc31b652f3a98c2392
Rifqi RIfaldi <rifqirifaldi94@gmail.com>
2025-09-10 13:28:25 +0700

    setup railway

--- main.go
+++ main.go
@@ -1,11 +1,7 @@
 package main
 
 import (
-	"log"
-	"os"
-
 	"github.com/gofiber/fiber/v2"
-	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/surisalbi/rest-api-go/controllers/authorcontroller"
 	"github.com/surisalbi/rest-api-go/controllers/bookcontroller"
 	"github.com/surisalbi/rest-api-go/controllers/membercontroller"
@@ -13,45 +9,27 @@ import (
 )
 
 func main() {
-	// koneksi database (di dalam models.ConnectDatabase kamu bisa ambil MYSQL_URL)
 	models.ConnectDatabase()
 
 	app := fiber.New()
-
-	// pakai env PORT dari Railway, fallback ke 3000 untuk local
-	port := os.Getenv("3306")
-	if port == "" {
-		port = "3000"
-	}
-
-	// middleware CORS
-	app.Use(cors.New(cors.Config{
-		AllowOrigins: "*",
-		AllowMethods: "GET,POST,PUT,DELETE",
-	}))
-
-	// routes books
+	
 	app.Get("/api/books", bookcontroller.Index)
 	app.Get("/api/books/:id", bookcontroller.Show)
-	app.Post("/api/books", bookcontroller.Create)
+	app.Post("/api/books/", bookcontroller.Create)
 	app.Put("/api/books/:id", bookcontroller.Update)
 	app.Delete("/api/books/:id", bookcontroller.Delete)
 
-	// routes authors
 	app.Get("/api/authors", authorcontroller.Index)
 	app.Get("/api/authors/:id", authorcontroller.Show)
-	app.Post("/api/authors", authorcontroller.Create)
+	app.Post("/api/authors/", authorcontroller.Create)
 	app.Put("/api/authors/:id", authorcontroller.Update)
 	app.Delete("/api/authors/:id", authorcontroller.Delete)
 
-	// routes members
 	app.Get("/api/members", membercontroller.Index)
 	app.Get("/api/members/:id", membercontroller.Show)
-	app.Post("/api/members", membercontroller.Create)
+	app.Post("/api/members/", membercontroller.Create)
 	app.Put("/api/members/:id", membercontroller.Update)
 	app.Delete("/api/members/:id", membercontroller.Delete)
 
-	// listen ke port Railway
-	log.Printf("Server running on port %s 🚀", port)
-	log.Fatal(app.Listen(":" + port))
+	app.Listen(":8000")
 }
--- models/setup.go
+++ models/setup.go
@@ -1,52 +1,25 @@
 package models
 
 import (
-	"fmt"
-	"log"
-	"net/url"
-	"os"
-
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
-// variabel global database
+// membuat variabel global untuk database
 var DB *gorm.DB
 
 func ConnectDatabase() {
-	// coba ambil dari Railway (MYSQL_URL)
-	mysqlURL := os.Getenv("mysql://root:cfQJevUGGDUfPbvfXhImdnTvCbiwdHTd@mysql.railway.internal:3306/railway")
+	// menghubungkan dengan database mysql
+	db, err := gorm.Open(mysql.Open("mysql://root:cfQJevUGGDUfPbvfXhImdnTvCbiwdHTd@mysql.railway.internal:3306/railway"))
 
-	var dsn string
-
-	if mysqlURL != "" {
-		// parsing MYSQL_URL -> mysql://user:pass@host:port/dbname
-		parsedURL, err := url.Parse(mysqlURL)
-		if err != nil {
-			log.Fatal("Invalid MYSQL_URL:", err)
-		}
-
-		user := parsedURL.User.Username()
-		pass, _ := parsedURL.User.Password()
-		host := parsedURL.Hostname()
-		port := parsedURL.Port()
-		dbname := parsedURL.Path[1:] // buang '/' di depan
-
-		dsn = fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
-			user, pass, host, port, dbname)
-	} else {
-		// fallback ke lokal
-		dsn = "root:root@tcp(localhost:8889)/rest_api_go?charset=utf8mb4&parseTime=True&loc=Local"
-	}
-
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	//cek koneksi ke database
 	if err != nil {
-		log.Fatal("Failed to connect to database:", err)
+		panic(err)
 	}
 
-	// migrasi tabel
-	db.AutoMigrate(&Book{}, &Author{}, &Member{})
-
+	// migrasi tabel jika tabel di database kosong
+	db.AutoMigrate(&Book{})
+	db.AutoMigrate(&Author{})
+	db.AutoMigrate(&Member{})
 	DB = db
-	log.Println("Database connected 🚀")
 }
